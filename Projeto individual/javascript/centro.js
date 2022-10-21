9c9183852d436604aa7784d803df56beb7b4a5b3
gabrielcostaferreira <114002000+gabrielcostaferreira@users.noreply.github.com>
2022-10-21 19:12:06 -0300

    commit do projeto individual

---
+++ Projeto individual/javascript/centro.js
@@ -0,0 +1,57 @@
+var metodo = document.querySelector(".metodo");
+var passo = document.querySelector(".passo");
+var enviar = document.querySelector(".enviar");
+var tipoRadio = document.forms[0].coddecod;
+var tipo = document.querySelector(".tipo");
+var chave = document.getElementById("chave").value;
+var entrada = document.querySelector("#entrada");
+
+passo.style.display = "none";
+metodo.addEventListener("change", function () {
+  metodo.style.backgroundColor = "#009eff";
+  metodo.style.transition = "none";
+  if (metodo.selectedIndex == 1) {
+    passo.style.display = "flex";
+  } else {
+    passo.style.display = "none";
+  }
+});
+
+tipo.addEventListener("change", function () {
+  
+  if (tipoRadio[0].checked) {
+    enviar.textContent = "Codificar";
+  } else {
+    enviar.textContent = "Decodificar";
+  }
+});
+
+passo.addEventListener("change", function () {
+ 
+  chave = document.getElementById("chave").value;
+});
+
+enviar.addEventListener("click", function (e) {
+  e.preventDefault();
+  var mensagem = entrada.value;
+  if (metodo.selectedIndex == 0) {
+    metodo.style.backgroundColor = "#ff6b6b";
+    setTimeout(
+      () => (metodo.style.backgroundColor = "#ff6b6b"),
+      1000
+    );
+    metodo.style.transition = "1s";
+  } else if (metodo.selectedIndex == 1) {
+    if (tipoRadio[0].checked) {
+      codificarCifra(mensagem, chave);
+    } else {
+      decodificarCifra(mensagem, chave);
+    }
+  } else {
+    if (tipoRadio[0].checked) {
+      codificarBase64(mensagem);
+    } else {
+      decodificarBase64(mensagem);
+    }
+  }
+});
